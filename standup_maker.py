dc027cd857d6e1527265ffa2943ac30b60caf5c3
Tom Meadows <thomasmeadows@spatialos.com>
2021-05-06 19:22:42 +0100

    Added standup maker python script

---
+++ standup_maker.py
@@ -0,0 +1,55 @@
+import argparse
+import os
+import json
+import sys
+import datetime
+from datetime import date
+
+def load_json(jsonfile):
+    """Read the "Daily" app json export file."""
+    try:
+        with open(os.path.join(sys.path[0], jsonfile), "r") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                raise json.JSONDecodeError(f"{jsonfile} is not valid JSON")
+    except IOError:
+        raise IOError(f"{jsonfile} does not exist. Please export it from the 'Daily' app.")
+
+def get_json_filename():
+    today = date.today()
+    today = date.strftime(today, '%d:%m:%Y')
+    standup_json = f"Daily\ Export\ \({today}\ -\ {today}\).json"
+    standup_json = standup_json.replace('\\', '')
+    return standup_json
+
+def write_standup_file(standup, output_file):
+    day = f"{date.today().strftime('%A')}:\n"
+    output_file.write(day)
+    for item in range(len(standup)):
+        duration = round(standup[item]['duration'], 2)
+        activity = standup[item]['activity']
+        entry = f"- [{duration}] {activity} \n"
+        output_file.write(entry)
+
+def main(standup_json, output_file):
+    standup = load_json(standup_json)
+    output_file = open(os.path.join(sys.path[0], 'output.txt'), 'a+')
+    output_file.truncate(0)
+    write_standup_file(standup, output_file)
+    output_file.close()
+    output_file = open(os.path.join(sys.path[0], 'output.txt'), 'a+')
+    output_file.seek(0)
+    print(output_file.read())
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Generates my standups file from json export from "Daily" app')
+    parser.add_argument('--standup', '-c', required=False, default=get_json_filename(),
+                        help='The location of the Platform Components YAML file')
+    parser.add_argument('--output', '-o', required=False, default='output.txt',
+                        help='The desired output location of the generated YAML manifest')
+    args = parser.parse_args()
+    main(args.standup, args.output)
